314fdb58084ba894e0ecc470df761528e0fc6c5e
Goat-QG <bcrespin@student.42.fr>
2016-05-31 20:46:46 +0200

    starting scene et level select

---
+++ Intheshadow/Assets/Script/ButtonBehav.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehav : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnDevClick()
+    {
+        Debug.Log("dev");
+        //Application.LoadLevel();
+    }
+
+    public void OnNormalClick()
+    {
+        Debug.Log("test");
+        //
+    }
+}
---
+++ Intheshadow/Assets/Script/Cameralvlselect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameralvlselect : MonoBehaviour {
+
+   private GameObject[] ArrayLevel;
+   private int Where = 0;
+	// Use this for initialization
+	void Start () {
+        ArrayLevel = GameObject.FindGameObjectsWithTag("Level");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (Where > )
+            Where++;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (Where > 0)
+            {
+                Where--;
+            }
+        }
+	}
+}
