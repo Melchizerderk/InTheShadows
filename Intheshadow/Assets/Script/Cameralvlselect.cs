492c4ce0c049a8cd1804ff0096545cc72bc3bb1b
Baptiste CRESPIN <bcrespin@e3r5p4.42.fr>
2016-06-01 17:24:09 +0200

    deplacement gauche droite sur le player select, debut du script de save/load, faire la classe qui contiendra ce qu il y a a sauvegarder

--- Intheshadow/Assets/Script/Cameralvlselect.cs
+++ Intheshadow/Assets/Script/Cameralvlselect.cs
@@ -1,28 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System;
 
-public class Cameralvlselect : MonoBehaviour {
-
-   private GameObject[] ArrayLevel;
-   private int Where = 0;
+public class Cameralvlselect : MonoBehaviour, IComparer {
+	
+	private GameObject[] Levels;
+	private int Where = 0;
+	private float savedy;
 	// Use this for initialization
+
+	int IComparer.Compare( System.Object x, System.Object y) {
+		return((new CaseInsensitiveComparer ()).Compare (((GameObject)x).name, ((GameObject)y).name));	
+	}
+
 	void Start () {
-        ArrayLevel = GameObject.FindGameObjectsWithTag("Level");
+        Levels = GameObject.FindGameObjectsWithTag("Level");
+		IComparer myComparer = new Cameralvlselect ();
+		Array.Sort (Levels, myComparer);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.D))
         {
-            if (Where > )
-            Where++;
+            if (Where < 3)
+			{
+				Where++;
+				savedy = gameObject.transform.position.y;
+				gameObject.transform.position = new Vector3(Levels[Where].transform.position.x, savedy, Levels[Where].transform.position.z);
+			}
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (Where > 0)
             {
-                Where--;
+				Where--;
+				savedy = gameObject.transform.position.y;
+				gameObject.transform.position = new Vector3(Levels[Where].transform.position.x, savedy, Levels[Where].transform.position.z);
             }
         }
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			//Application.LoadLevel(Level[Where].name);
+		}
 	}
 }
