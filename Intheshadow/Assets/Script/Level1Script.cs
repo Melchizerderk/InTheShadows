87452f5fbab1c347fe1ffb2585d013fceaa1965f
Baptiste CRESPIN <bcrespin@e3r9p2.42.fr>
2016-06-15 14:57:12 +0200

    level1 et level2 fonctionne tres bien juste faire le global maintenant, vais ptet avoir un souci avec la montee de niveau

---
+++ Intheshadow/Assets/Script/Level1Script.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Script : MonoBehaviour {
+
+	// Use this for initialization
+	private Quaternion WinPos;
+	private bool havewon = false;
+	public GameObject LvlCanvas;
+
+	void Start () {
+		WinPos.x = gameObject.transform.rotation.x;
+		WinPos.y = 1;
+		LvlCanvas.GetComponent<CanvasGroup>().alpha = 0;
+		LvlCanvas.GetComponent<CanvasGroup>().interactable = false;
+		LvlCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+			transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+		if (Quaternion.Angle (WinPos, gameObject.transform.rotation) < 10) {
+			StartCoroutine(WinWaitTime(3));
+			if (havewon == true)
+			{
+				LvlCanvas.GetComponent<CanvasGroup>().alpha = 1;
+				SaveProfile();
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel("LevelSelect");
+		}
+	}
+
+	IEnumerator WinWaitTime(int wtime){
+		yield return new WaitForSeconds (wtime);
+		if (Quaternion.Angle (WinPos, gameObject.transform.rotation) < 10)
+			havewon = true;
+		else
+			havewon = false;
+	}
+
+	void SaveProfile()
+	{
+		if (GameControl.control.Mode == 1) {
+			if (GameControl.control.PlayerLevel == 0)
+			{
+				GameControl.control.PlayerLevel = 1;
+				GameControl.control.Save();
+			}
+		}
+	}
+}
---
+++ Intheshadow/Assets/Script/Level2Script.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2Script : MonoBehaviour {
+
+	private Quaternion WinPos;
+	private bool havewon = false;
+	public GameObject LvlCanvas;
+	
+	void Start () {
+		WinPos.x = 1;
+		WinPos.y = gameObject.transform.rotation.x;
+		LvlCanvas.GetComponent<CanvasGroup>().alpha = 0;
+		LvlCanvas.GetComponent<CanvasGroup>().interactable = false;
+		LvlCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(1))
+			transform.Rotate(Input.GetAxis("Mouse Y"), 0, 0);
+		Debug.Log (Quaternion.Angle (WinPos, gameObject.transform.rotation));
+		if (Quaternion.Angle (WinPos, gameObject.transform.rotation) > 85 && Quaternion.Angle (WinPos, gameObject.transform.rotation) < 100) {
+			StartCoroutine(WinWaitTime(3));
+			if (havewon == true)
+			{
+				LvlCanvas.GetComponent<CanvasGroup>().alpha = 1;
+				SaveProfile();
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel("LevelSelect");
+		}
+	}
+	
+	IEnumerator WinWaitTime(int wtime){
+		yield return new WaitForSeconds (wtime);
+		if (Quaternion.Angle (WinPos, gameObject.transform.rotation) > 85 && Quaternion.Angle (WinPos, gameObject.transform.rotation) < 100)
+			havewon = true;
+		else
+			havewon = false;
+	}
+	
+	void SaveProfile()
+	{
+		if (GameControl.control.Mode == 1) {
+			if (GameControl.control.PlayerLevel == 1)
+			{
+				GameControl.control.PlayerLevel = 2;
+				GameControl.control.Save();
+			}
+		}
+	}
+}
